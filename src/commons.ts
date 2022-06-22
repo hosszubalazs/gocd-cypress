57af6c88d159de18d02dafe65ccb437c5397a76c
rkrisztian <2960445+rkrisztian@users.noreply.github.com>
2022-06-22 11:00:53 +0200

    Remove `CY_INTEGRATION_FOLDER`.
    This is an unused feature now.

--- src/commons.ts
+++ src/commons.ts
@@ -1,6 +1,5 @@
 import execa from 'execa';
 import path from 'path';
-import fs from 'fs';
 
 const PROJECT_PACKAGE_JSON_PROPERTY = 'gocdCypress';
 
@@ -16,20 +15,6 @@ function config(envVarName: string, defaultValue: string): string {
 export const CY_PROJECT_PATH = config('CY_PROJECT_PATH', process.cwd());
 export const CY_DOCKER_IMAGE = config('CY_DOCKER_IMAGE', 'cypress/browsers:node16.13.0-chrome95-ff94');
 export const CY_BOOTSTRAP_COMMAND = config('CY_BOOTSTRAP_COMMAND', 'true');
-
-export const CY_INTEGRATION_FOLDER = (() => {
-	let cypressDefinedIntegrationFolder = undefined;
-
-	const cypressJsonFile = path.resolve(CY_PROJECT_PATH, 'cypress.json');
-	if (fs.existsSync(cypressJsonFile)) {
-		const cypressJsonContent = fs.readFileSync(cypressJsonFile, 'utf8');
-		cypressDefinedIntegrationFolder = JSON.parse(cypressJsonContent).integrationFolder as (string | undefined);
-	}
-	return config(
-		'CY_INTEGRATION_FOLDER',
-		cypressDefinedIntegrationFolder || path.resolve(CY_PROJECT_PATH, 'cypress/integration')
-	);
-})();
 
 export function exec(command: string, options?: execa.Options): execa.ExecaChildProcess {
 
